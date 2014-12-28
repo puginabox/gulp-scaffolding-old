106dfec54ef93750a4ab28ea3bc6d4f21c6716e7
Griffin Byron <puginabox@gmail.com>
2014-12-28 15:43:53 -0500

    setting up gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,17 @@
 
 //require is a node.js method
-var gulped = require('gulp'); //assign the gulp library to gulped variable
-
-// log is a gulp-util method
+var gulped = require('gulp'),       //assign the gulp library to gulped variable
+    gutil = require('gulp-util'),    //assign the gulp-util library to gulped variable
+//    sass = require('sass'),
+    
+    // destintations plugged into variables for clarity
+    jsSources = ['js/*.js', 'js/file2.js', 'js/file3.js'];
+    
+; 
 gulped.task('welcome', function(){
-    console.log('Gulp ready to go');
+    gutil.log('Gulp ready to go'); // log is a gulp-util method
 });
+//gulped.task('task1', function(){
+//    gulped.src(jsSources); // src method as input
+//          .pipe(sass());
+//});
